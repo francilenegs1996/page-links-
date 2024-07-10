c74f2fcf5ae2db88b28651aeca7892442c98577f
francilenegs1996 <136385190+francilenegs1996@users.noreply.github.com>
2024-07-10 15:47:23 -0300

    first_commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { BackgroundContainer, ContainerText, Title } from "./components/Cardlinks/Container";
+import {
+  BackgroundContainer,
+  Button,
+  ContainerText,
+  CtaText,
+  
+  
+  SubTitle,
+  Title,
+} from "./components/Cardlinks/Container";
 
 
 
 export default function App() {
   return (
-   <BackgroundContainer>
-    <ContainerText>
-      <Title>PARE AGORA MESMO DE CONFIAR NA SORTE!</Title>
-    </ContainerText>
-   </BackgroundContainer>
+    <BackgroundContainer>
+      <ContainerText>
+        <Title>PARE AGORA MESMO DE CONFIAR NA SORTE!</Title>
+        <SubTitle>
+          E GARANTA O SUCESSO DE SUA EMPRESA, COM ASSESSORIA DE MARKETING DE
+          QUEM É ESPECIALISTA EM CAPTAÇÃO DE LEADS E JÁ TEM MAIS DE 10 ANOS DE
+          EXPERIÊNCIA NO MERCADO. 
+        </SubTitle>
+        <CtaText>CONFIE NA EXPERIÊNCIA, E NÃO NA SORTE!</CtaText>
+        <Button  href ="https://www.figma.com/" >CLIQUE AQUI!</Button>
+      </ContainerText>
+    </BackgroundContainer>
+    
   );
 }
--- src/components/Cardlinks/Container.ts
+++ src/components/Cardlinks/Container.ts
@@ -1,22 +1,32 @@
 import styled from 'styled-components';
 
 export const BackgroundContainer = styled.div`
-   width: 100%;
+  width: 100%;
   height: 100vh;
   display: flex;
-   align-items: center;
-   background-size: cover;
-      background-position: center;
-   background-image: url('/src/assets/bg.svg');
+  align-items: center;
+  background-position: center;
+  background-repeat: no-repeat;
 
-   @media (max-width: 768px) {
-    background-image: url('/src/assets/bg mobile.svg');
-    background-size: contain;
-    background-repeat: no-repeat;;
-    position: relative; 
-    
+  opacity: 0.89;
+      
+  background-image: url('/src/assets/bg.svg');
 
-}
+  @media (max-width: 768px) {
+  // background-image: url('/src/assets/bg mobile.svg');
+  }
+
+  @media (orientation: portrait) {
+    background-size: auto 100%;
+    background-size: cover;
+    background-position: 80% 50%;
+  }
+
+  @media (orientation: landscape) {
+    background-size: 100% auto;
+  }
+
+
 `;
 
 
@@ -25,8 +35,11 @@ display: flex;
 width: 701px;
 flex-direction: column;
 align-items: flex-start;
-gap: 24px;
+gap: 28px;
 padding-left: 60px;
+padding-right: 60px;
+
+
   
 `;
 
@@ -37,6 +50,7 @@ font-size: 56px;
 font-style: normal;
 font-weight: 700;
 line-height: 120%;
+filter: drop-shadow(-4px 6px 43px #000000);
 
 @media (max-width: 768px) {
     font-size: 40px;
@@ -44,3 +58,75 @@ line-height: 120%;
   }
 `;
 
+export const SubTitle = styled.h2`
+color: #D9D9D9;
+
+font-family: Inter;
+font-size: 18px;
+font-style: normal;
+font-weight: 200;
+line-height: 150%; /* 27px */
+filter: drop-shadow(-4px 6px 41px #000000);
+
+`;
+
+export const CtaText = styled.h2`
+color: white;
+
+font-family: Inter;
+font-size: 18px;
+font-style: normal;
+font-weight: 600;
+line-height: 150%; /* 27px */
+filter: drop-shadow(-4px 6px 43px #000000);
+
+`;
+
+export const Button = styled.a`
+color: white;
+display: inline-block;
+ padding: 0.9rem 1.8rem;
+ font-size: 16px;
+ font-weight: 700;
+ color: #072A50;
+ cursor: pointer;
+ position: relative;
+ background-color: white;
+ text-decoration: none;
+ overflow: hidden;
+ z-index: 1;
+ font-family: inherit;
+ border-radius: 10px;
+ transition: 0.2s;
+ filter: drop-shadow(-4px 6px 41px #000000);
+
+ &:hover {
+  background: #072A50;
+  transition: 0.2s;
+  color: white;
+}
+`;
+
+
+
+ 
+
+export const SocialContainer = styled.a`
+  width: 52px;
+  height: 52px;
+  background-color: rgb(44, 44, 44);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  transition-duration: .3s;
+  border-radius: 2px;
+  
+  &:hover {
+    background-color: #d62976;
+    transition-duration: .3s;
+  
+    
+}
+`
+;
